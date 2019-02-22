d039dd0b0a70082ac3958630eefae35115b822b1
Wesley Heron <wfarias.ti@gmail.com>
2019-02-22 15:27:10 +0000

    .

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-let dev_db_url = 'mongodb://u_deliciouspizza:pizza000@ds347665.mlab.com:47665/deliciouspizza';
-//let dev_db_url = 'mongodb://u_deliciouspizza:pizza000@ds347665.mlab.com:47665/deliciouspizza';
-//let dev_db_url = 'mongodb://<dbuser>:<dbpassword>@ds347665.mlab.com:47665/deliciouspizza';
+
+let dev_db_url = 'mongodb://<dbuser>:<dbpassword>@ds347665.mlab.com:47665/deliciouspizza';
 
 let mongoDB = process.env.MONGODB_URI || dev_db_url;
 
