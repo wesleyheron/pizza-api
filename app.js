666421f0fd8dc81016524e8348f66b2765486c26
Wesley Heron <wfarias.ti@gmail.com>
2019-02-22 15:22:55 +0000

    .

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/pizza', pizza);
 
-let port = 5000;
+let port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log('Server is up and running on port number: ' + port);
